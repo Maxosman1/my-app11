5393e8af27f3fd07ceceefddb5d053c3c41264f3
Maxosman1 <120883114+Maxosman1@users.noreply.github.com>
2024-02-07 20:55:30 -0600

    1

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import { createClient } from '@supabase/supabase-js';
 import { Auth, Typography, Container } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 
-const supabase = createClient('your-supabase-url', 'your-supabase-anon-key');
+const supabase = createClient('https://uwixomogyvygqonywfqz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3aXhvbW9neXZ5Z3Fvbnl3ZnF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA0MTgzNjQsImV4cCI6MjAxNTk5NDM2NH0.hAb1Q6wiv4JbAPLprFAeopOa3-Eizf9w8Hasg7JCmvo');
 
 const queryClient = new QueryClient();
 
