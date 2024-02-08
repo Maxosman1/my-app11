287a42a170429d761c551174b536e5a19d642213
Maxosman1 <120883114+Maxosman1@users.noreply.github.com>
2024-02-07 21:43:55 -0600

    1

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ import ContestSubmissionsPage from './Contests/Submissions';
 import JoinContest from './Contests/JoinContest';
 import Profile from './Auth/Profile';
 import EditProfile from './Auth/EditProfile';
-import Contact from './contact';
+import Contact from './contact';f
 import Leaderboard from './Leaderboard';
 import Login from './Auth/Login'; // Assuming you have a Login component
 import ReviewPage from './ReviewPage/Reviewpage'; // Import the ReviewPage component
 
 import { createClient } from '@supabase/supabase-js';
-import { Auth, Typography, Container } from '@supabase/auth-ui-react';
+import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 const supabase = createClient('https://uwixomogyvygqonywfqz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3aXhvbW9neXZ5Z3Fvbnl3ZnF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA0MTgzNjQsImV4cCI6MjAxNTk5NDM2NH0.hAb1Q6wiv4JbAPLprFAeopOa3-Eizf9w8Hasg7JCmvo');
